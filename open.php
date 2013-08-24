49da7dff23d4151ba0695953dada2f3201322c47
TeMPOraL <temporal.pl@gmail.com>
2013-08-24 05:44:50 +0200

    first time

---
+++ open.php
@@ -0,0 +1,13 @@
+<?php
+
+define('API_SOURCE_URL', "http://spaceapi.hskrk.pl/");
+
+function hsOpen() {
+
+//TODO error handling against json_decode
+//
+//$APIResult = 
+//$decodedResult = json_decode($APIResult)
+//$state = $decodedResult['state']
+    return $state;
+}
